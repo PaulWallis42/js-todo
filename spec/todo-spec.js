9737f72c17abcce6ae6da2517f0b526dfce81e83
paulwallis42 <paulwallis42@gmail.com>
2017-04-18 12:29:00 +0100

    todo item removed and added to completed items list test pass

--- spec/todo-spec.js
+++ spec/todo-spec.js
@@ -7,12 +7,26 @@ describe('Todo', function(){
   });
 
   it('can store todo items', function(){
-    expect(todo.items).toEqual([]);
+    expect(todo.todoItems).toEqual([]);
   });
 
-  it('todo items can be added to storage', function(){
+  it('can add items to todos', function(){
     todo.addItem('Walk Dog');
-    expect(todo.items[0]).toEqual('Walk Dog');
+    expect(todo.todoItems[0]).toEqual('Walk Dog');
+  });
+
+  it('can remove specific items from todos', function(){
+    todo.addItem('Walk Dog');
+    todo.addItem('Do Laundry');
+    todo.removeItem('Walk Dog')
+    expect(todo.todoItems.length).toEqual(1);
+    expect(todo.todoItems[0]).toEqual('Do Laundry');
+  });
+
+  it('adds removed items to the completed list', function(){
+    todo.addItem('Walk Dog');
+    todo.removeItem('Walk Dog');
+    expect(todo.completedItems[0]).toEqual('Walk Dog');
   });
 
 });
--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,16 @@
 function Todo(){
-  this.items = [];
+  this.todoItems = [];
+  this.completedItems = [];
 }
 
-Todo.prototype.addItem = function (item) {
-  this.items.push(item);
-};
+Todo.prototype.addItem = function(item) {
+  this.todoItems.push(item);
+}
+
+Todo.prototype.removeItem = function(item){
+  var index = this.todoItems.indexOf(item);
+  if (index >= 0){
+    var removed = this.todoItems.splice(index, 1);
+  }
+  this.completedItems.push(removed[0]);
+}
