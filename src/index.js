f1591c39c6517c383ded44853947a972ac5776fe
paulwallis42 <paulwallis42@gmail.com>
2017-04-19 09:25:56 +0100

    add new todos, refresh form and format ul children

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 window.onload = function(){
 
+  var todo = new Todo();
+  displayTodos();
+
+  function displayTodos(){
+    var todoList = document.getElementById('todo');
+    while (todoList.hasChildNodes()){
+      todoList.removeChild(todoList.firstChild);
+    }
+    todo.todoItems.forEach(function(item){
+      var newLi = document.createElement('li');
+      newLi.innerText = item;
+      todoList.appendChild(newLi);
+    });
+  }
+
+  document.getElementById('main-form').onsubmit = function(evt){
+    evt.preventDefault();
+    var item = document.getElementById('new-todo').value;
+    todo.addItem(item);
+    document.getElementById('main-form').reset();
+    displayTodos();
+  }
 
 
-
-
-};
+}
